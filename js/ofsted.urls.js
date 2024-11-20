26f9406b62f08fa672c895a62bed0ecf36545b47
FlexaGroup <162819959+FlexaGroup@users.noreply.github.com>
2024-11-20 22:36:02 +0000

    full careers update

--- js/ofsted.urls.js
+++ js/ofsted.urls.js
@@ -1,17 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Get all anchor tags with a .html extension and remove it
-    var anchors = document.querySelectorAll("a[href$='.html']");
-    anchors.forEach(function (anchor) {
-        anchor.setAttribute(
-            "href",
-            anchor.getAttribute("href").replace(".html", "")
-        );
-    });
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Get all anchor tags with a .html extension and remove it
+//     var anchors = document.querySelectorAll("a[href$='.html']");
+//     anchors.forEach(function (anchor) {
+//         anchor.setAttribute(
+//             "href",
+//             anchor.getAttribute("href").replace(".html", "")
+//         );
+//     });
 
-    // Remove .html from the current page's URL
-    var currentPageUrl = window.location.href;
-    if (currentPageUrl.endsWith(".html")) {
-        var updatedUrl = currentPageUrl.replace(".html", "");
-        window.history.replaceState({}, document.title, updatedUrl);
-    }
-});
+//     // Remove .html from the current page's URL
+//     var currentPageUrl = window.location.href;
+//     if (currentPageUrl.endsWith(".html")) {
+//         var updatedUrl = currentPageUrl.replace(".html", "");
+//         window.history.replaceState({}, document.title, updatedUrl);
+//     }
+// });
